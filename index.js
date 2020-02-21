75c449f74fe0b1583c44234c51d6afe60f8341a1
littleonetwo <littleonetwo@gmail.com>
2020-02-20 18:17:20 -0600

    added nodemon and dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+require('dotenv').config();
 //setup our middleware requirements
 const express = require('express');
 const helmet = require('helmet');
@@ -7,6 +8,7 @@ const logger = require('./middleware/logger.js');
 
 //setup our server
 const server = express();
+const port = process.env.PORT || 5000;
 
 //add routers
 const postsRoutes = require('./posts/postRouter.js');
@@ -29,4 +31,4 @@ server.use(function(req, res) {
 })
 
 //choose a port to work out of should be at end of file
-server.listen(8050, () => console.log('API running on port 8050'));
+server.listen(port, () => console.log(`\n***Server running on http://localhost:${port}***\n`));
